9c6537ca5d59528e84534eff4d21a9caf1bd44f8
Romain <romain@geev.fr>
2019-05-31 18:39:50 +0200

    Initialize sqlite datatable

--- config.py
+++ config.py
@@ -1,3 +1,8 @@
+import os
+
 SECRET_KEY = "NlArl(BRZydZocL)ay`,Y7)*"
 
 FB_APP_ID = 457179715088100
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "app.db")
